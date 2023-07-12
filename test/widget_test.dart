644b5b802e4370fe646d86b03d7314235abb9147
KOTOKOU Komla David <kotokou74@gmail.com>
2023-07-12 00:14:10 +0000

    update

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,12 +8,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:tmdb_app/main.dart';
+import 'package:tmdb_app/screens/splash_screen.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(SplashScreen(
+      onInitializationComplete: () {},
+    ));
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
